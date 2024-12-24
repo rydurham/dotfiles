8f8508f14f4095f0add604639f5a28d1cee268fd
Ryan C. Durham <ryan@stagerightlabs.com>
2024-12-23 20:42:54 -0800

    Add pactl sink toggle alias

--- bashrc.d/205.audio-aliases.sh
+++ bashrc.d/205.audio-aliases.sh
@@ -1,1 +1,14 @@
 alias linein='pactl load-module module-loopback'
+
+# https://askubuntu.com/a/740002
+sink() {
+    new_sink=$(pacmd list-sinks | grep index | tee /dev/stdout | grep -m1 -A1 "* index" | tail -1 | cut -c12-)
+
+    echo "Setting default sink to: $new_sink";
+    pacmd set-default-sink $new_sink
+    pacmd list-sink-inputs | grep index | while read line
+    do
+    pacmd move-sink-input `echo $line | cut -f2 -d' '` $new_sink
+    done
+
+}
---
+++ scripts/toggle-pactl-sink.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e
+
+# https://askubuntu.com/a/740002
+new_sink=$(pacmd list-sinks | grep index | tee /dev/stdout | grep -m1 -A1 "* index" | tail -1 | cut -c12-)
+
+echo "Setting default sink to: $new_sink";
+pacmd set-default-sink $new_sink
+pacmd list-sink-inputs | grep index | while read line
+do
+pacmd move-sink-input `echo $line | cut -f2 -d' '` $new_sink
+done
