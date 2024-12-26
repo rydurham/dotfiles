cb04deb08ce00c9bf50f7de20ca138d5ee9aac77
Ryan C. Durham <ryan@stagerightlabs.com>
2024-12-26 14:54:56 -0800

    Add IP fetching alias

--- bashrc.d/201.general-aliases.sh
+++ bashrc.d/201.general-aliases.sh
@@ -9,3 +9,6 @@ toggleTheme() {
 }
 
 alias theme=toggleTheme
+
+# https://unix.stackexchange.com/questions/22615/how-can-i-get-my-external-ip-address-in-a-shell-script/81699#81699
+alias ip='dig @resolver4.opendns.com myip.opendns.com +short'
