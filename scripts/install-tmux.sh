473d0413886d704cae81a7a6996e307d7b81656b
Ryan C. Durham <ryan@stagerightlabs.com>
2025-01-17 15:23:21 -0800

    Update tmux installation script

--- scripts/install-tmux.sh
+++ scripts/install-tmux.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt install build-essential libevent-dev libncurses-dev -y
+sudo apt install build-essential libevent-dev libncurses-dev yacc -y
 
-wget https://github.com/tmux/tmux/releases/download/3.4/tmux-3.4.tar.gz
+wget https://github.com/tmux/tmux/releases/download/3.5/tmux-3.5.tar.gz
 
-tar -zxvf tmux-3.4.tar.gz
-cd tmux-3.4
+tar -zxvf tmux-3.5.tar.gz
+cd tmux-3.5
 ./configure
 make
 sudo make install
 cd ..
-rm -rf tmux-3.4
-rm tmux-3.4.tar.gz
+rm -rf tmux-3.5
+rm tmux-3.5.tar.gz
