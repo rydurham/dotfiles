090ede35e431fae7976ddb73688cc7589e1f606c
Ryan C. Durham <ryan@stagerightlabs.com>
2025-07-31 10:15:13 -0700

    config updates

--- scripts/configure-git.sh
+++ scripts/configure-git.sh
@@ -3,7 +3,7 @@
 # Configure Git
 git config --global user.name "Ryan C. Durham"
 git config --global user.email ryan@stagerightlabs.com
-git config --global core.editor "vim"
+git config --global core.editor "nvim -f"
 git config --global push.default simple
 git config --global pull.rebase false
 git config --global init.defaultBranch main
